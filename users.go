7646db203a0f67a4494d762b25f1968498d74b42
Andrew Archibald <andrew@aarchibald.com>
2020-05-06 18:47:06 -0700

    Select between cred and users.tar.gz

--- users.go
+++ users.go
@@ -12,11 +12,33 @@ import (
 )
 
 func main() {
-	f, err := os.Open("users.tar.gz")
+	var filename string
+	if len(os.Args) >= 2 {
+		filename = os.Args[1]
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			fmt.Fprintf(os.Stderr, "%s not found\n", filename)
+			os.Exit(2)
+		}
+	} else if _, err := os.Stat("cred"); err == nil {
+		filename = "cred"
+	} else if _, err := os.Stat("users.tar.gz"); err == nil {
+		filename = "users.tar.gz"
+	} else {
+		fmt.Fprintln(os.Stderr, "cred or users.tar.gz not found")
+		os.Exit(2)
+	}
+
+	f, err := os.Open(filename)
 	try(err)
 	defer f.Close()
-	r, err := NewTarGZReader(f)
-	try(err)
+
+	var r io.Reader = f
+	if strings.HasSuffix(filename, ".tar.gz") || strings.HasSuffix(filename, ".tgz") {
+		tr, err := NewTarGZReader(f)
+		try(err)
+		r = tr
+	}
+
 	s := NewRowScanner(r)
 	for {
 		row, err := s.Row()
