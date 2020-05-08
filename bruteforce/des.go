a52f5a87b8bf1513a19be29c0135f7d2b505d68c
Andrew Archibald <andrew@aarchibald.com>
2020-05-07 18:35:32 -0700

    Concurrently search DES keys

--- bruteforce/des.go
+++ bruteforce/des.go
@@ -4,31 +4,63 @@ import (
 	"crypto/des"
 	"encoding/binary"
 	"fmt"
+	"runtime"
+	"sync"
 	"time"
 )
 
+var cipher = [8]byte{0x2f, 0xca, 0x9b, 0x00, 0x3d, 0xe3, 0x97, 0x78}
+var plain = [8]byte{'p', 'a', 's', 's', 'w', 'o', 'r', 'd'}
+
 // DES brute force of all 56-bit keys.
 func main() {
-	cipher := [8]byte{0x2f, 0xca, 0x9b, 0x00, 0x3d, 0xe3, 0x97, 0x78}
-	plain := [8]byte{'p', 'a', 's', 's', 'w', 'o', 'r', 'd'}
 	start := time.Now()
-	t := start
-	for i := uint64(0); i < 1<<56; i += 1 << 24 {
-		key, ok, err := desWorker(i, i+1<<24, cipher, plain)
-		if err != nil {
-			panic(err)
-		}
-		if ok {
-			fmt.Printf("Key found in %v: %x\n", time.Since(start), key)
-			break
-		}
-		now := time.Now()
-		fmt.Printf("%d keys tried, %v %v\n", i+1<<24, now.Sub(start), now.Sub(t))
-		t = now
+
+	k := uint64(0)
+	done := false
+	var mu sync.Mutex
+
+	workers := runtime.GOMAXPROCS(-1)
+	fmt.Printf("Searching with %d workers\n", workers)
+
+	var wg sync.WaitGroup
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			for {
+				mu.Lock()
+				if k >= 1<<56 || done {
+					mu.Unlock()
+					break
+				}
+				min, max := k, k+1<<24
+				k = max
+				mu.Unlock()
+
+				key, ok, err := desSearchRange(min, max)
+				if err != nil {
+					panic(err)
+				}
+
+				if ok {
+					fmt.Printf("Found key %x in %v\n", key, time.Since(start))
+					mu.Lock()
+					done = true
+					mu.Unlock()
+					break
+				}
+				fmt.Printf("Searched %x to %x, %v elapsed\n", min, max, time.Since(start))
+			}
+
+			wg.Done()
+		}()
+
+		time.Sleep(500) // space out workers
 	}
+	wg.Wait()
 }
 
-func desWorker(min, max uint64, cipher, plain [8]byte) (uint64, bool, error) {
+func desSearchRange(min, max uint64) (uint64, bool, error) {
 	var key [8]byte
 	var out [8]byte
 	for i := min; i < max; i++ {
