440fa62422b6c345f438a4a76d9aa32a38fcde9f
Andrew Archibald <andrew@aarchibald.com>
2020-05-09 16:44:50 -0700

    Add profiling to DES brute force

--- bruteforce/des.go
+++ bruteforce/des.go
@@ -3,9 +3,13 @@ package main
 import (
 	"encoding/binary"
 	"flag"
-	"fmt"
+	"log"
+	"os"
+	"os/signal"
 	"runtime"
+	"runtime/pprof"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/andrewarchi/adobe-cred/des"
@@ -15,9 +19,10 @@ var (
 	cipher uint64 = 0x2fca9b003de39778
 	plain  uint64 = binary.BigEndian.Uint64([]byte("password"))
 
-	start uint64
-	end   uint64
-	step  uint64
+	start      uint64
+	end        uint64
+	step       uint64
+	cpuProfile string
 )
 
 // DES brute force of all 56-bit keys.
@@ -25,14 +30,32 @@ func main() {
 	flag.Uint64Var(&start, "start", 0, "starting search bound")
 	flag.Uint64Var(&end, "end", 1<<56, "ending search bound")
 	flag.Uint64Var(&step, "step", 1<<24, "search increment")
+	flag.StringVar(&cpuProfile, "cpuprofile", "", "write cpu profile to file")
 	flag.Parse()
+
+	if cpuProfile != "" {
+		f, err := os.Create(cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-c
+			pprof.StopCPUProfile()
+			os.Exit(1)
+		}()
+	}
 
 	k := start
 	done := false
 	var mu sync.Mutex
 
 	workers := runtime.GOMAXPROCS(-1)
-	fmt.Printf("Searching with %d workers\n", workers)
+	log.Printf("Searching with %d workers\n", workers)
 	t0 := time.Now()
 
 	var wg sync.WaitGroup
@@ -56,14 +79,14 @@ func main() {
 				}
 
 				if ok {
-					fmt.Printf("Found key 0x%x (0x%x) in %v\n", key, des.Intersperse56(key), time.Since(t0))
+					log.Printf("Found key 0x%x (0x%x) in %v\n", key, des.Intersperse56(key), time.Since(t0))
 					mu.Lock()
 					done = true
 					mu.Unlock()
 					break
 				}
 				now := time.Now()
-				fmt.Printf("Searched 0x%x to 0x%x in %v, %v elapsed\n", min, max, now.Sub(t), now.Sub(t0))
+				log.Printf("Searched 0x%x to 0x%x in %v, %v elapsed\n", min, max, now.Sub(t), now.Sub(t0))
 			}
 
 			wg.Done()
